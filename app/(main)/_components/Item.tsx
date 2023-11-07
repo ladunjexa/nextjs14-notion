c1659c3b0cdbebea2e0cb416aa730f901f748d8d
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-08 01:02:11 +0200

    Modified styles for main-components to reslove `darkMode` issues

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -113,7 +113,7 @@ const Item = ({
       {!!id && (
         <div
           role="button"
-          className="mr-1 h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          className="mr-1 h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
           onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -94,14 +94,14 @@ const TrashBox = (props: Props) => {
               <div
                 role="button"
                 onClick={(e) => onRestore(e, document._id)}
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>
               <ConfirmModal onConfirm={() => onRemove(document._id)}>
                 <div
                   role="button"
-                  className="rounded-sm p-2 hover:bg-neutral-200"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
                   <Trash className="h-4 w-4 text-muted-foreground" />
                 </div>
