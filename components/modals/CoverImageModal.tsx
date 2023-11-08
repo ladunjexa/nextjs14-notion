ebca6fe1ac4b7b0edb4feeaebedda4d425076cc5
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-08 18:07:55 +0200

    Implemented image dropzone functionality within cover and integrated it into `CoverImageModal` modal-component. Installed @react-dropzone ^14.2.3 package

--- components/modals/CoverImageModal.tsx
+++ components/modals/CoverImageModal.tsx
@@ -2,9 +2,46 @@
 
 import { Dialog, DialogHeader, DialogContent } from "../ui/dialog";
 import { useCoverImage } from "@/hooks/useCoverImage";
+import { SingleImageDropzone } from "../shared/SingleImageDropzone";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -12,7 +49,12 @@ export const CoverImageModal = () => {
         <DialogHeader>
           <h2 className="text-center text-lg font-semibold">Cover Image</h2>
         </DialogHeader>
-        <div>TODO: Upload image</div>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
