10b17db07404355c611befff1274543f2eb9424a
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-09 01:00:04 +0200

    Created a read-only preview route for a public documents

---
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import React, { useMemo } from "react";
+import Cover from "@/components/shared/Cover";
+import Toolbar from "@/components/shared/Toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+
+type Props = {
+  params: {
+    documentId: Id<"documents">;
+  };
+};
+
+const Page = ({ params: { documentId } }: Props) => {
+  const Editor = useMemo(
+    () =>
+      dynamic(() => import("@/components/shared/Editor"), {
+        ssr: false,
+      }),
+    []
+  );
+
+  const update = useMutation(api.documents.update);
+  const document = useQuery(api.documents.getById, {
+    documentId,
+  });
+
+  const onChange = (content: string) => {
+    update({
+      id: documentId,
+      content,
+    });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found...</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <Cover preview url={document.coverImage} />
+      <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+        <Toolbar preview initialData={document} />
+        <Editor
+          editable={false}
+          onChange={onChange}
+          initialContent={document.content}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
