9821b8050d14059019082439e5e71942a322a7a8
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-09 02:44:18 +0200

    Refactored Next.js config to utilize `images.remotePatterns` for domain specification, in alignment with the deprecation of `images.domains`

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["files.edgestore.dev"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+      },
+      {
+        protocol: "http",
+        hostname: "files.edgestore.dev",
+      },
+    ],
   },
 };
 
